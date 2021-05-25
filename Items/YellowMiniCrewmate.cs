b9423d052c100da1298d51940619a91679db99ca
Alstro20 <sb74851@gmail.com>
2021-05-25 11:26:54 -0500

    Added sprites for all colors

---
+++ Items/YellowMiniCrewmate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorialMod.Items
+{
+    class YellowMiniCrewmate : CyanMiniCrewmate
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Yellow Mini Crewmate");
+        }
+    }
+}
