b6808690549ec73e742476482d51e6341f6a8a2c
Alstro20 <sb74851@gmail.com>
2021-05-27 16:38:00 -0500

    Added blue crewmate hat functionality
    Note: currently using cyan's texture, because messing with cyan code would be problematic and cyan's texture was already done.

--- Items/MiniCrewmates/BlueMiniCrewmate.cs
+++ Items/MiniCrewmates/BlueMiniCrewmate.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
 
 namespace MiniCrewmates.Items.MiniCrewmates
 {
+    [AutoloadEquip(EquipType.Head)]
     class BlueMiniCrewmate : CyanMiniCrewmate
     {
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Blue Mini Crewmate");
+            Tooltip.SetDefault("Now in hat form!");
+        }
+
+        public override void SetDefaults()
+        {
+            item.vanity = true;
         }
     }
 }
